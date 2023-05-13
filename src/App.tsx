2ead250a137d2c848b62b1e06560a7cfc506f298
nttduonggg <duongntt19406@st.uel.edu.vn>
2023-05-13 16:45:23 +0700

    feat: install packages and create new css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Footer } from './components/Footer'
+import Header from './components/Header'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <div>
+      <Header/>
+      <RouterProvider router={router}></RouterProvider>
+      <Footer/>
+    </div>
+  )
 }
 
 export default App
