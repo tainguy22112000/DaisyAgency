7df504720cfc830c5baf54f834e2df61c1ed7fdb
nttduonggg <duongntt19406@st.uel.edu.vn>
2023-05-13 16:46:51 +0700

    feat: add pages routes and logo/icons

---
+++ src/page/GenericError/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GenericError() {
+  return (
+    <div>GenericError</div>
+  )
+}
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -7,9 +7,7 @@ import Header from '~/components/Header'
 export const HomePage = () => {
   return (
     <div>
-      <Header />
-      <Banner />
-      <Footer />
+      HomePage
     </div>
   )
 }
