2d86228b23cc6ab425f91feffa8e71a550a90466
nttduonggg <duongntt19406@st.uel.edu.vn>
2023-05-13 18:32:02 +0700

    feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import styles from './styles.module.scss'
+import PrimaryButton from '../PrimaryButton'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <img src="src\assets\images\Logo.png" alt="Logo" />
+        </div>
+        <div className={styles.seperated}></div>
+        <div className={styles.navigation}>
+          <ul>
+            <li><a href="#" className={styles.active}>Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Solutions & Services</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.navbar}>
+        <PrimaryButton title="Get in touch"/>
+      </div>
+    </div>
+  )
 }
 
 export default Header
