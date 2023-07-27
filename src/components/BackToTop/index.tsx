465d3cea42eb64fb5ac8852c98f2d12882f4cbaf
Admin <nttduong.c3tqcap.a1@gmail.com>
2023-07-27 22:21:36 +0700

    feat: refactor styles for BackToTop button

--- src/components/BackToTop/index.tsx
+++ src/components/BackToTop/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FaChevronUp } from 'react-icons/fa'
 import styles from './styles.module.scss'
+import classNames from 'classnames';
 
 export default function BackToTop() {
     const [visible, setVisible] = useState<boolean>(false);
@@ -15,18 +16,17 @@ export default function BackToTop() {
         }
       };
 
-      const scrollToTop = () =>{
-        window.scrollTo({
-          top: 0, 
-          behavior: 'smooth'
-        });
-      };
+    const scrollToTop = () =>{
+      window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+      });
+    };
       
-      window.addEventListener('scroll', toggleVisible);
+    window.addEventListener('scroll', toggleVisible);
   return (
-    <button className={styles.backToTop} style={{display: visible ? 'inline' : 'none'}} >
-        <FaChevronUp onClick={scrollToTop} 
-        />
-   </button>
+   <button className={classNames(styles.backToTop, !visible && styles.invisible)} >
+      <FaChevronUp onClick={scrollToTop} />
+    </button>
   )
 }
