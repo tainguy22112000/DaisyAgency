0cfacf12ef2c4cf7ef71c1404ef71227858a4830
Admin <nttduong.c3tqcap.a1@gmail.com>
2023-08-18 13:47:54 +0700

    feat: implement UI for the rest of News page

---
+++ src/constants/News.tsx
@@ -0,0 +1,77 @@
+export const newsSlider = [{
+    date: 'Sep 6, 2022',
+    title: 'This is our first date',
+    description: 'Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.'
+}, {
+    date: 'Sep 6, 2023',
+    title: 'And this is our 1 year anniversary',
+    description: 'Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.'
+}, {
+    date: 'Nov 22, 2023',
+    title: 'Your upcomming birthday',
+    description: 'Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.'
+}, {
+    date: 'Jan 6, 2023',
+    title: 'This is my very first birthday with you',
+    description: 'Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.'
+}, {
+    date: 'Dec 24, 2022',
+    title: 'And lastly is our first Christmas together',
+    description: 'Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.'
+}];
+
+export const newsList = [{
+    imgPath: 'src/assets/images/NewsImg01.png',
+    altText: 'News image 01',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg02.png',
+    altText: 'News image 02',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg03.png',
+    altText: 'News image 03',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg04.png',
+    altText: 'News image 04',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg05.png',
+    altText: 'News image 05',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg06.png',
+    altText: 'News image 06',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg07.png',
+    altText: 'News image 07',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg08.png',
+    altText: 'News image 08',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}, {
+    imgPath: 'src/assets/images/NewsImg09.png',
+    altText: 'News image 09',
+    date: 'Oct 10, 2023',
+    title: 'System maintenance',
+    shortDescription: 'PSYS constantly invests, deploys and integrates more and more tools and new technology applications to ensure stable and fast operation of the system.'
+}];
--- src/page/News/index.tsx
+++ src/page/News/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.scss'
-import { Heading1, Heading2, Paragraph } from '~/components/Typography'
+import { Heading1, Heading2, Heading3, Paragraph } from '~/components/Typography'
 import { PrimaryButton, SecondaryButton } from '~/components/Button'
 import Spacer from '~/components/Spacer'
 import classNames from 'classnames'
+import { newsList, newsSlider } from '~/constants/News'
+import Card from '~/components/Card'
 
 export default function News() {
+  const [slide, setSlide] = useState(0);
+    
+  const handleSetSlide = (key: number) => {
+    setSlide(key);
+  }
+
   return (
     <div className={styles.container}>
 
@@ -26,35 +34,77 @@ export default function News() {
           </ul>
         </div>
 
-        {/* Carousel - UI */}
-        <div className={styles.carousel}>
+        {/* Slider - UI */}
+        <div className={styles.sliders}>
           <img src="src\assets\images\NewsBanner.png" alt="First-banner" />
           <div className={styles.designButton}>
             <PrimaryButton title="Design" />
           </div>
-          <div className={styles.newsWrapper}>
-            <div>
-              <Paragraph>Oct 10, 2022</Paragraph>
+        {newsSlider.map((news, key) => (
+          <div className={slide === key ? styles.activeSlide : styles.hiddenSlide}>
+            <div key={key}>
+              <Paragraph>{news.date}</Paragraph>
               <Spacer size="2xl" />
-              <Heading2>But this article will not deal with that sense of the word</Heading2>
+              <Heading2>{news.title}</Heading2>
               <Spacer size="2xl" />
-              <Paragraph>Inhabit hearing perhaps on ye do no. It maids decay as there he. Smallest on suitable disposed do although blessing he juvenile in. Society or if excited forbade. Here name off yet she long sold easy whom. Differed oh cheerful procured pleasure securing suitable in. Hold rich on an he oh fine. Chapter ability shyness article welcome be do on service.</Paragraph>
+              <Paragraph>{news.description}</Paragraph>
               <Spacer size="2xl" />
               <div className={styles.readMoreButton}>
                 <SecondaryButton title="Read more" />
               </div>
             </div>
           </div>
+        ))}
         </div>
 
-        {/* Carousel Display Bar */}
-          <div className={styles.carouselDisplayBar}>
-            <div className={classNames(styles.carouselElement, styles.display)}></div>
-            <div className={styles.carouselElement}></div>
-            <div className={styles.carouselElement}></div>
-            <div className={styles.carouselElement}></div>
-            <div className={styles.carouselElement}></div>
+        {/* Slider Indicator */}
+        <div className={styles.indicators}>
+          {newsSlider.map((_, key) => (
+            <div 
+              key={key} 
+              className={slide === key ? styles.activeIndicator : classNames(styles.inactiveIndicator, styles.activeIndicator)} 
+              onClick={() => handleSetSlide(key)}></div>
+          ))}
+        </div>
+        <hr className={styles.seperatedLine} />
+
+        {/* News */}
+        <div className={styles.newsList}>
+          {newsList.map((news, key) => (
+            <Card padding="sm" key={key}>
+              <img src={news.imgPath} alt={news.altText} />
+              <Spacer size="lg" />
+              <Paragraph>{news.date}</Paragraph>
+              <Spacer size="lg" />
+              <Heading3>{news.title}</Heading3>
+              <Spacer size="lg" />
+              <Paragraph>{news.shortDescription}</Paragraph>
+            </Card>
+          ))}
+          <div className={styles.viewMoreBtn}>
+            <SecondaryButton title="View more" />
           </div>
+        </div>
+
+        {/* Contact */}
+      <div className={styles.contact}>
+          <div className={styles.contactForm}>
+              <Heading2>Need more information or want to get in touch?</Heading2>
+              <Spacer size="md"/>
+              <Paragraph>Enter your email to get the latest product information, promotions and more</Paragraph>
+              <form action="#">
+                <input type="text" placeholder='Enter your name' className={styles.formInput} />
+                <Spacer size="xl"/>
+                <input type="text" placeholder='Enter your name' className={styles.formInput} />
+                <Spacer size="xl"/>
+                <textarea name="form-message" id="form-id" rows={6} placeholder='Your message' className={styles.formInput}></textarea>
+              </form>
+              <PrimaryButton title="Confirm"/>
+          </div>
+          <div>
+            <img src="src\assets\images\MoreInfoImg.png" alt="more-infor-image" />
+          </div>
+      </div>
     </div>
   )
 }
